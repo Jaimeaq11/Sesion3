69cdf15b859263fc3dd13fcd90eacdcd84c791b8
Jaime <Jaime@DESKTOP-6JUEDSB>
2025-10-07 18:29:03 +0200

    commit 1

---
+++ Sesion3/src/pkg/Cuenta.java
@@ -0,0 +1,52 @@
+package pkg;
+
+public class Cuenta {
+	
+	String numero;
+	String titular;
+	Double saldo;
+	
+	public Cuenta(String numero) {
+		this.numero = numero;
+	}
+	
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getTitular() {
+		return titular;
+	}
+
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+
+	public Double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(Double saldo) {
+		this.saldo = saldo;
+	}
+
+	public Cuenta(double saldo) {
+		// TODO Auto-generated constructor stub
+		this.saldo = saldo;
+	}
+
+	public void ingresar(Double i) {
+		// TODO Auto-generated method stub
+		this.saldo += i;
+	}
+
+	public void retirar(Double i) {
+		// TODO Auto-generated method stub
+		this.saldo -= i;
+	}
+	
+}
---
+++ Sesion3/src/test/CuentaTest.java
@@ -0,0 +1,34 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import pkg.Cuenta;
+
+class CuentaTest {
+	
+	private static Cuenta cuenta;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		cuenta = new Cuenta(0);
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		cuenta.setSaldo(0.0);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+}
